d0c8ed111a99f0cc774462ae625e4fea5b53c15e
Vigneshwaran16 <iamvignesh16@gmail.com>
2022-12-29 16:05:09 +0530

    :art: Move custom element styles from light to shadow dom

--- src/Sidebar/SidebarMenuItem/sidebar-menu-item.view.js
+++ src/Sidebar/SidebarMenuItem/sidebar-menu-item.view.js
@@ -2,13 +2,11 @@ export default (() => {
     return `
     <link rel="stylesheet" href="./src/Sidebar/SidebarMenuItem/sidebar-menu-item.styles.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-    <div id="dropdown-container">
-        <div id="dropdown-menu">
-            <span></span>
-            <i class="fas fa-caret-right"></i>
-        </div>
-        <div id="dropdown-list" is-active="false" is-empty="true">
-        </div>
-    </div
+    <div id="dropdown-menu">
+        <span></span>
+        <i class="fas fa-caret-right"></i>
+    </div>
+    <div id="dropdown-list" is-active="false" is-empty="true">
+    </div>
     `
 })
